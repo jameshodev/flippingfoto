b541ab4efa70e065917660b94845cef252aab02e
James Ho <jamesho.dev@gmail.com>
2021-09-23 16:56:08 -0700

    render timeline photos

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
@@ -5,10 +6,19 @@ import usePhotos from '../hooks/use-photos';
 function Timeline() {
   // get the logged in user's photos (hook)
   const { photos } = usePhotos();
-  console.log('photos', photos);
+  // on loading the photos, we need to use react skeleton
+  // if we have photos, render them (creat a post componet)
+  // if the user has no photos, tell them to create some photos
   return (
     <div className="container col-span-2">
-      <p>I am the Timeline</p>
+      {/* load skeleton when the photos hasn't loaded yet */}
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
     </div>
   );
 }
